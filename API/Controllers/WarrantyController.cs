5950f9de72759a26c9ad6a11942a5afc99733a7c
Alex <alex.champkin.97@gmail.com>
2020-07-31 12:02:02 +0100

    Made endpoints return some data

--- API/Controllers/WarrantyController.cs
+++ API/Controllers/WarrantyController.cs
@@ -1,10 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System;
 
 namespace API.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
     public class WarrantyController : ControllerBase
     {
         private readonly ILogger<WarrantyController> _logger;
@@ -15,15 +15,25 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public string Info()
+        [Route("warranty/info")]
+        public IActionResult Info()
         {
-            return "I'm a warranty bot, beep beep. It's in warranty, trust me!";
+            var info = new
+            {
+                AppliesTo = "All models",
+                MonthOfLifeFrom = new DateTime(2020, 01, 01),
+                MonthOfLifeTo = new DateTime(2022, 12, 31),
+                MileageUpTo = 100000
+            };
+
+            return new JsonResult(info);
         }
 
         [HttpGet]
-        public bool HasWarranty()
+        [Route("warranty/status")]
+        public IActionResult Status()
         {
-            return false;
+            return new JsonResult("In warranty");
         }
     }
 }
